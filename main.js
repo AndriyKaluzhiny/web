e30d84d7d88d9a4475e938caac64e9a3fe18add6
Asus <kalujnyj.andrij@gmail.com>
2025-06-03 19:52:16 +0300

    working tree clean

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 function getSystemInfo() {
   return {
-    platform: process.platform,
-    arch: process.arch,
-    version: process.version,
-    uptime: process.uptime(),
-    memoryUsage: process.memoryUsage(),
+    platform: navigator.userAgent
   };
 }
 
@@ -12,15 +8,60 @@ function saveSystemInfoToLocalStorage() {
   const systemInfo = getSystemInfo();
   localStorage.setItem('systemInfo', JSON.stringify(systemInfo));
 
-  document.getElementById('output').textContent = JSON.stringify(systemInfo, null, 2);
+  document.getElementById('platform').textContent = systemInfo.platform;
 }
 
-document.onload = function() {
-  // Check if the browser supports localStorage
-  if (typeof(Storage) !== "undefined") {
-    saveSystemInfoToLocalStorage();
-    console.log("System information saved to localStorage.");
-  } else {
-    console.error("Local storage is not supported by this browser.");
-  }
-}   
+window.onload = function() { 
+    const variantNumber = 10;
+    const url = `https://jsonplaceholder.typicode.com/posts/${variantNumber}/comments`;
+
+     fetch(url)
+      .then(response => response.json())
+      .then(comments => {
+        const list = document.getElementById('comments-list');
+        comments.forEach(comment => {
+          const li = document.createElement('li');
+          li.innerHTML = `<strong>${comment.name}</strong> (${comment.email}):<br>${comment.body}`;
+          list.appendChild(li);
+        });
+      })
+      .catch(error => {
+        console.error('Помилка при отриманні коментарів:', error);
+      });
+}
+
+function closeModal() {
+  const modal = document.getElementById('feedbackModal');
+  modal.style.display = 'none';
+}
+
+setTimeout(() => {
+    document.getElementById('feedbackModal').style.display = 'block'},
+     60000);
+
+
+function changeTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-theme');;
+    
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+    } else {
+        body.classList.add('dark-mode');
+    }
+}
+
+function autoApplyTheme() {
+    const currentHour = new Date().getHours();
+    const body = document.body;
+
+    if (currentHour >= 7 && currentHour < 21) {
+        body.classList.remove('dark-mode');
+    } else {
+        body.classList.add('dark-mode');
+    }
+}
+
+window.onload = function() {
+    autoApplyTheme();
+}
